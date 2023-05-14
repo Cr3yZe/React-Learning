e4bd83533dc3646d51611db45413ca44f4203c21
Cr3yZe <addy_czr@yahoo.com>
2023-05-14 12:27:03 +0300

    adding another page

---
+++ Section 20/01-starting-project/src/pages/Products.js
@@ -0,0 +1,11 @@
+const ProductsPage = () => {
+  return <ul>
+    <li>Product 1</li>
+    <li>Product 2</li>
+    <li>Product 3</li>
+    <li>Product 4</li>
+    <li>Product 5</li>
+  </ul>
+};
+
+export default ProductsPage;
